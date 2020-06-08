120bd17d6a31fbdfc0a57874e025ce46b67dc2ab
John Bouchard <jbouchard13@gmail.com>
2020-06-07 23:31:50 -0400

    starter question will swap to different questions array depending on input

--- Develop/app.js
+++ Develop/app.js
@@ -14,36 +14,125 @@ const teamMembers = [];
 
 // create arrays of questions for each role
 // array of starter questions to prompt basic employee info
-const starterQuestions = [
+const starterQuestion = {
+  type: "list",
+  name: "role",
+  message: "What is the team member's role?",
+  choices: ["Manager", "Engineer", "Intern"],
+};
+const managerQuestions = [
   {
     type: "input",
     name: "name",
-    message: "What is the team member's name?",
+    message: "What is the manager's name?",
   },
   {
     type: "input",
     name: "id",
-    message: "What is the team member's id?",
+    message: "What is the manager's employee id?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is the manager's email address?",
+  },
+  {
+    type: "input",
+    name: "officeNumber",
+    message: "What is the manager's office number?",
   },
   {
     type: "list",
-    name: "role",
-    message: "What is the team member's role?",
-    choices: ["Manager", "Engineer", "Intern"],
+    name: "add",
+    message: "What role do you want to add?",
+    choices: ["Manager", "Engineer", "Intern", "I do not want to add another"],
+  },
+];
+const engineerQuestions = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is the engineer's name?",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is the engineer's employee id?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is the engineer's email address?",
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "What is the engineer's github username?",
+  },
+  {
+    type: "list",
+    name: "add",
+    message: "What role do you want to add?",
+    choices: ["Manager", "Engineer", "Intern", "I do not want to add another"],
+  },
+];
+const internQuestions = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is the intern's name?",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is the intern's employee id?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is the intern's email address?",
+  },
+  {
+    type: "input",
+    name: "school",
+    message: "What is the intern's school?",
+  },
+  {
+    type: "list",
+    name: "add",
+    message: "What role do you want to add?",
+    choices: ["Manager", "Engineer", "Intern", "I do not want to add another"],
   },
 ];
 
-// take the input from the first two questions
+// take the input from the first question
 // to determine which ones to prompt next
-inquirer.prompt(starterQuestions).then((answers) => {
-  if (answers.choices === "Manager") {
+inquirer.prompt(starterQuestion).then((answers) => {
+  if (answers.role === "Manager") {
     getManagerInfo();
-  } else if (answers.choices === "Engineer") {
+  } else if (answers.role === "Engineer") {
     getEngineerInfo();
-  } else if (answers.choices === "Intern") {
+  } else if (answers.role === "Intern") {
     getInternInfo();
   }
 });
+// create functions to get info for each employee type
+
+// manager info function
+const getManagerInfo = () => {
+  // run inquirer to prompt info for a new manager
+  inquirer.prompt(managerQuestions).then((answers) => {
+    // create a new manager with the answered name, id, email, and office number
+    const manager = new Manager(
+      answers.name,
+      answers.id,
+      answers.email,
+      answers.officeNumber
+    );
+    console.log(manager);
+  });
+};
+
 // pass info from teamMembers array to the render function
 // const newHtml = render(teamMembers);
 // pass rendered data into writeFileSync
